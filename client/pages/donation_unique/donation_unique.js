2cf902aab390f22a8420104a4ca663d5accdec77
MeritWayler <eugeniomartinez92@gmail.com>
2016-11-03 13:25:15 -0500

    erased suscription and unique

--- client/pages/donation_unique/donation_unique.js
+++ client/pages/donation_unique/donation_unique.js
@@ -1,1 +1,22 @@
 
+
+
+// Template.payment_method.events({
+
+//   "click #package1": function(){
+//   	$("#package1").css("color", "red");
+//   }
+//   "click #package2": function(){
+//   	$("#package1").css("color", "red");
+//   }
+//   "click #package3": function(){
+//   	$("#package1").css("color", "red");
+//   }
+//   "click #package4": function(){
+//   	$("#package1").css("background-color", "red");
+//   }
+
+// });
+$("#package1").click(function(){
+    $("#package1").css("color", "red");
+});
