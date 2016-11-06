ca2410d92cd0d21438175f1ac997df098f6fee95
MeritWayler <eugeniomartinez92@gmail.com>
2016-11-06 14:52:29 -0600

    finished JS for unique donation and copied everything to suscription and adapted

--- client/pages/donation_unique/donation_unique.js
+++ client/pages/donation_unique/donation_unique.js
@@ -1,10 +1,14 @@
 Template.donation_unique.rendered = function(){
 	Session.set("specifyDonation", false);
+	Session.set("donationSelected", false);
 }
 
 Template.donation_unique.helpers({
 	isSpecifiedDonation: function(){
 		return Session.get("specifyDonation");
+	},
+	isDonationSelected: function(){
+		return Session.get("donationSelected");
 	}
 });
 
@@ -14,15 +18,33 @@ Template.donation_unique.events({
 	},
 	"click #donate50": function(e){
 		Session.set("specifyDonation", false);
+		Session.set("donationSelected", true);
 		Session.set("donateAmount", 50);
 	},
 	"click #donate100": function(e){
 		Session.set("specifyDonation", false);
+		Session.set("donationSelected", true);
 		Session.set("donateAmount", 100);
 	},
 	"click #donate200": function(e){
 		Session.set("specifyDonation", false);
+		Session.set("donationSelected", true);
 		Session.set("donateAmount", 200);
+	},
+	"click #nextBtnOther": function(e){
+		Session.set("donationSelected", false);
+		var Amount = $("#amount").val();
+	    if(Amount===""){
+	      $("#amount").addClass("invalid");
+	      Materialize.toast("Porfavor ingresa la cantidad que desea donar",4000);
+	    }
+	    else if(isNaN(Amount)){
+	    	$("#amount").addClass("invalid");
+	      Materialize.toast("La cantidad que ingresaste no es válida",4000);
+	    }
+
+
+		Session.set("donateAmount", Amount);
 	}
 });
 
